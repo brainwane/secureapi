ccb9a4aa1f8b9adf88b343ac90cdd0afa096e8da
Sumana Harihareswara <sumanah@panix.com>
2014-11-06 13:49:47 -0500

    parsing request and sending it back

--- hhserver.py
+++ hhserver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import BaseHTTPServer
+import subprocess
+import os
 
 PORT = 8000
 
@@ -13,14 +15,26 @@ class APIHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.content_length = int(self.headers.get("Content-Length","0"))
         self.request_body = self.rfile.read(self.content_length)
         # Determine our response body
-        self.response_body = self.request_body #just echo the request body
+        self.response_body = parse_request(self.request_body)
         # Send our response
         self.send_response(200)
         self.send_header("Content-Length", str(len(self.response_body)))
         self.end_headers()
         self.wfile.write(self.response_body)
 
-
+def parse_request(request_body):
+    """ take a request from the client, return the string to
+    be used in the response body
+    """
+    with open("bar.c", "w") as f:
+        f.write(request_body)
+    try:
+        clang_result = subprocess.check_output(["scan-build", "clang", "-Weverything", "bar.c"], stderr=subprocess.STDOUT, universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        clang_result = e.output
+    os.remove("bar.c")
+    return clang_result
+    # TODO: create a tempfile, randomly generated name
 
 def main(server_class=BaseHTTPServer.HTTPServer,
         handler_class=APIHTTPRequestHandler):
