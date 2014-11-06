b7408e75f031e5b8246bec135a9053d692e30bc5
Greg Hendershott <greghendershott@gmail.com>
2014-11-06 15:01:27 -0500

    Fix regexp for `header`.
    Also:
    
    - Add `summary?` and use it to exclude the summary lines such as
      "7 warnings generated".
    
    - Add some comments.

--- parse.rkt
+++ parse.rkt
@@ -2,13 +2,21 @@
 
 (require json)
 
+;; If `s` the start of an error item, return a hash of the elements,
+;; else return #f.
 (define (header s)
   (match s
-    [(pregexp "^([^:]+):(\\d+):(\\d+)\\s+(.*)$"
+    [(pregexp "^([^:]+):(\\d+):(\\d+):\\s+(.*)$"
               (list _ file line col desc))
      (hash 'file file 'line line 'col col 'desc desc)]
     [_ #f]))
             
+;; Is `s` a summary line such as "3 warnings generated."?
+(define (summary? s)
+  (match s
+    [(pregexp "^\\d+ warnings? generated.") #t]
+    [_ #f]))
+
 (define (gather-by f? xs)
   (match xs
     [(cons x xs)
@@ -22,8 +30,9 @@
 
 (define (parse s)
   (define xs (regexp-split "\n" s))
-  (define ys (gather-by header xs))
-  (write-json ys))
+  (define ys (filter (negate summary?) xs))
+  (define zs (gather-by header ys))
+  (write-json zs))
 
 ;; (parse (file->string "example.txt"))
 
